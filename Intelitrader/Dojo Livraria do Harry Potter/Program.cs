7e4f80333cc7273f79efeb1ada5c804d131e930a
Gustavo Moreno <gustavosilva.moreno2003@gmail.com>
2021-02-01 02:22:51 -0300

    final

---
+++ Intelitrader/Dojo Livraria do Harry Potter/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace Dojo_Livraria_do_Harry_Potter {
+    class Program {
+        static void Main (string[] args) {
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine ("___________.Bem Vindo(a) Livraria do Harry Potter.____________");
+            Console.ResetColor ();
+
+            double livro = 42.00;
+            double desconto;
+            double quantidade;
+            double pf, cs, pa, cf, of , ep, rm;
+
+            System.Console.WriteLine ("Quantas copias de Pedra Filosofal você quer:");
+            pf = double.Parse (Console.ReadLine ());
+            System.Console.WriteLine ("Quantas copias de Câmara Secreta você quer:");
+            cs = double.Parse (Console.ReadLine ());
+            System.Console.WriteLine ("Quantas copias de Prisioneiro de Azkaban você quer:");
+            pa = double.Parse (Console.ReadLine ());
+            System.Console.WriteLine ("Quantas copias de Cálice de Fogo você quer:");
+            cf = double.Parse (Console.ReadLine ());
+            System.Console.WriteLine ("Quantas copias de Ordem da Fênix você quer:"); 
+            of = double.Parse (Console.ReadLine ());
+            System.Console.WriteLine ("Quantas copias de Enigma do Príncipe você quer:");
+            ep = double.Parse (Console.ReadLine ());
+            System.Console.WriteLine ("Quantas copias de Relíquias da Morte você quer:");
+            rm = double.Parse (Console.ReadLine ());
+
+            
+
+                //=========================================== Pedra Filosofal========================================================
+                if (pf == 2) {
+                    quantidade = livro * 2;
+                    desconto = quantidade * (1 - 0.05);
+                    System.Console.WriteLine ("O valor que você ira pagar em Pedra Filosofal é de:" + desconto);
+                } else if (pf == 3) {
+                    quantidade = livro * 3;
+                    desconto = quantidade * (1 - 0.10);
+                    System.Console.WriteLine ("O valor que você ira pagar em Pedra Filosofal é de:" + desconto);
+                } else if (pf == 4) {
+                    quantidade = livro * 4;
+                    desconto = quantidade * (1 - 0.15);
+                    System.Console.WriteLine ("O valor que você ira pagar em Pedra Filosofal é de:" + desconto);
+                } else if (pf == 5) {
+                    quantidade = livro * 5;
+                    desconto = quantidade * (1 - 0.20);
+                    System.Console.WriteLine ("O valor que você ira pagar em Pedra Filosofal é de:" + desconto);
+                }else if( pf < 0 || pf > 5){
+                    System.Console.WriteLine(" O maximo de livros que vendemos são 5 e o minimo são 0");
+                }
+                
+
+            // ==============================================Câmara Secreta==============================================================
+                if (cs == 2) {
+                    quantidade = livro * 2;
+                    desconto = quantidade * (1 - 0.05);
+                    System.Console.WriteLine ("O valor que você ira pagar em Câmara Secreta é de:" + desconto);
+                } else if (cs == 3) {
+                    quantidade = livro * 3;
+                    desconto = quantidade * (1 - 0.10);
+                    System.Console.WriteLine ("O valor que você ira pagar em Câmara Secreta é de:" + desconto);
+                } else if (cs == 4) {
+                    quantidade = livro * 4;
+                    desconto = quantidade * (1 - 0.15);
+                    System.Console.WriteLine ("O valor que você ira pagar em Câmara Secreta é de:" + desconto);
+                } else if (cs == 5) {
+                    quantidade = livro * 5;
+                    desconto = quantidade * (1 - 0.20);
+                    System.Console.WriteLine ("O valor que você ira pagar em Câmara Secreta é de:" + desconto);
+                }else if( cs < 0 || cs > 5){
+                    System.Console.WriteLine(" O maximo de livros que vendemos são 5 e o minimo são 0");
+                }
+
+                // ==============================================Prisioneiro de Azkaban==============================================================
+                if (pa == 2) {
+                    quantidade = livro * 2;
+                    desconto = quantidade * (1 - 0.05);
+                    System.Console.WriteLine ("O valor que você ira pagar em Prisioneiro de Azkaban é de:" + desconto);
+                } else if (pa == 3) {
+                    quantidade = livro * 3;
+                    desconto = quantidade * (1 - 0.10);
+                    System.Console.WriteLine ("O valor que você ira pagar em Prisioneiro de Azkaban é de:" + desconto);
+                } else if (pa == 4) {
+                    quantidade = livro * 4;
+                    desconto = quantidade * (1 - 0.15);
+                    System.Console.WriteLine ("O valor que você ira pagar em Prisioneiro de Azkaban é de:" + desconto);
+                } else if (pa == 5) {
+                    quantidade = livro * 5;
+                    desconto = quantidade * (1 - 0.20);
+                    System.Console.WriteLine ("O valor que você ira pagar em Prisioneiro de Azkaban é de:" + desconto);
+                }else if( pa < 0 || pa > 5){
+                    System.Console.WriteLine("O maximo de livros que vendemos são 5 e o minimo são 0");
+                }
+
+                // ==============================================Cálice de Fogo==============================================================
+                if (cf == 2) {
+                    quantidade = livro * 2;
+                    desconto = quantidade * (1 - 0.05);
+                    System.Console.WriteLine ("O valor que você ira pagar em Cálice de Fogo é de:" + desconto);
+                } else if (cf == 3) {
+                    quantidade = livro * 3;
+                    desconto = quantidade * (1 - 0.10);
+                    System.Console.WriteLine ("O valor que você ira pagar em Cálice de Fogo é de:" + desconto);
+                } else if (cf == 4) {
+                    quantidade = livro * 4;
+                    desconto = quantidade * (1 - 0.15);
+                    System.Console.WriteLine ("O valor que você ira pagar em Cálice de Fogo é de:" + desconto);
+                } else if (cf == 5) {
+                    quantidade = livro * 5;
+                    desconto = quantidade * (1 - 0.20);
+                    System.Console.WriteLine ("O valor que você ira pagar em Cálice de Fogo é de:" + desconto);
+                }else if( cf < 0 || cf > 5){
+                    System.Console.WriteLine("O maximo de livros que vendemos são 5 e o minimo são 0");
+                }
+
+                // ==============================================Ordem da Fênix==============================================================
+                if (of == 2) {
+                    quantidade = livro * 2;
+                    desconto = quantidade * (1 - 0.05);
+                    System.Console.WriteLine ("O valor que você ira pagar em Ordem da Fênix é de:" + desconto);
+                } else if (of == 3) {
+                    quantidade = livro * 3;
+                    desconto = quantidade * (1 - 0.10);
+                    System.Console.WriteLine ("O valor que você ira pagar em Ordem da Fênix é de:" + desconto);
+                } else if (of == 4) {
+                    quantidade = livro * 4;
+                    desconto = quantidade * (1 - 0.15);
+                    System.Console.WriteLine ("O valor que você ira pagar em Ordem da Fênix é de:" + desconto);
+                } else if (of == 5) {
+                    quantidade = livro * 5;
+                    desconto = quantidade * (1 - 0.20);
+                    System.Console.WriteLine ("O valor que você ira pagar em Ordem da Fênix é de:" + desconto);
+                }else if( of < 0 || of > 5){
+                    System.Console.WriteLine("O maximo de livros que vendemos são 5 e o minimo são 0");
+                }
+
+                // ==============================================Enigma do Príncipe==============================================================
+                if (ep == 2) {
+                    quantidade = livro * 2;
+                    desconto = quantidade * (1 - 0.05);
+                    System.Console.WriteLine ("O valor que você ira pagar em Enigma do Príncipe é de:" + desconto);
+                } else if (ep == 3) {
+                    quantidade = livro * 3;
+                    desconto = quantidade * (1 - 0.10);
+                    System.Console.WriteLine ("O valor que você ira pagar em Enigma do Príncipe é de:" + desconto);
+                } else if (ep == 4) {
+                    quantidade = livro * 4;
+                    desconto = quantidade * (1 - 0.15);
+                    System.Console.WriteLine ("O valor que você ira pagar em Enigma do Príncipe é de:" + desconto);
+                } else if (ep == 5) {
+                    quantidade = livro * 5;
+                    desconto = quantidade * (1 - 0.20);
+                    System.Console.WriteLine ("O valor que você ira pagar em Enigma do Príncipe é de:" + desconto);
+                }else if( ep < 0 || ep > 5){
+                    System.Console.WriteLine("O maximo de livros que vendemos são 5 e o minimo são 0");
+                }
+
+                // ==============================================Relíquias da Morte==============================================================
+                if (rm == 2) {
+                    quantidade = livro * 2;
+                    desconto = quantidade * (1 - 0.05);
+                    System.Console.WriteLine ("O valor que você ira pagar em Relíquias da Morte é de:" + desconto);
+                } else if (rm == 3) {
+                    quantidade = livro * 3;
+                    desconto = quantidade * (1 - 0.10);
+                    System.Console.WriteLine ("O valor que você ira pagar em Relíquias da Morte é de:" + desconto);
+                } else if (rm == 4) {
+                    quantidade = livro * 4;
+                    desconto = quantidade * (1 - 0.15);
+                    System.Console.WriteLine ("O valor que você ira pagar em Relíquias da Morte é de:" + desconto);
+                } else if (rm == 5) {
+                    quantidade = livro * 5;
+                    desconto = quantidade * (1 - 0.20);
+                    System.Console.WriteLine ("O valor que você ira pagar em Relíquias da Morte é de:" + desconto);
+                }else if( rm < 0 || rm > 5){
+                    System.Console.WriteLine(" O maximo de livros que vendemos são 5 e o minimo são 0");
+                }
+            
+            
+
+        }
+    }
+}
---
+++ Intelitrader/Dojo Troco/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Dojo_Troco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("-------------BemVindo(a) ao Caixa Eletronico --------- XD");
+            Console.ResetColor();
+
+           int t100,t50,t10,t5,t1,m5,m25,m10,m05,m1;
+            double pago,troco,produ,falt;
+
+            troco = 0.0;
+            produ = 0.0;
+            pago = 0.0;
+            falt = 0.0;
+            t100 = 0;
+            t50 = 0;
+            t10 = 0;
+            t5 = 0;
+            t1 = 0;
+            m5 = 0;
+            m25 = 0;
+            m10 = 0;
+            m05 = 0;
+            m1 = 0;
+            
+
+            Console.WriteLine("Digite o valor do produto:");
+            produ = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o pagamento");
+            pago = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            
+
+            if (produ > pago)
+            {
+                falt = produ - pago;
+                Console.WriteLine("Valor a ser pago: R$ {0}", produ);
+                Console.WriteLine("Valor pago: R$ {0}", pago);
+                Console.WriteLine("Faltam: R$ {0}",falt);
+            }
+
+            else if (produ == pago)
+            {
+                Console.WriteLine("Valor a ser pago: R$ {0}", produ);
+                Console.WriteLine("Valor pago: R$ {0}", pago);
+                Console.WriteLine("Não existe troco!");
+            }
+
+            else
+                troco = pago - produ;
+            { 
+                while (troco >= 100)
+                {                   
+                    troco -= 100;                    
+                    t100++;                   
+                }
+
+                while (troco >= 50)
+                {                   
+                    troco -= 50;                    
+                    t50++;                   
+                }
+                while (troco >= 10)
+                {                   
+                    troco -= 10;                    
+                    t10++;                   
+                }
+
+                while (troco >= 5)
+                {                   
+                    troco -= 5;                   
+                    t5++;
+                }
+
+                while (troco >=1)
+                {                    
+                    troco -= 1;                    
+                    t1++;                   
+                }
+                
+                while (troco >= 0.50)
+                {                    
+                    troco -= 0.50;                   
+                    m5++;                   
+                }
+                
+                while (troco >= 0.25)
+                {                    
+                    troco -= 0.25;                    
+                    m25++;                    
+                }
+                
+                while (troco >= 0.10)
+                {                    
+                    troco -= 0.10;                    
+                    m10++;                   
+                }
+
+                while (troco >= 0.05)
+                {
+                    troco -= 0.05;
+                    m05++;
+                }
+
+                while (troco >= 0.01)
+                {
+                    troco -= 0.01;
+                    m1++;  
+                }
+
+                if (pago > produ) Console.WriteLine("Valor a ser pago: R$ {0}", produ);
+                if (pago > produ) Console.WriteLine("Valor pago: R$ {0}", pago);
+                if (pago > produ) Console.WriteLine("Valor do troco: R$ {0}", (pago-produ));
+                if (t100 > 0) Console.WriteLine("{0} Nota(s) de: R$ 100,00 reais", t100);
+                if (t50 > 0) Console.WriteLine("{0} Nota(s) de: R$ 50,00 reais", t50);
+                if (t10 > 0) Console.WriteLine("{0} Nota(s) de: R$ 10,00 reais", t10);
+                if (t5 > 0) Console.WriteLine("{0} Nota(s) de: R$ 5,00 reais", t5);
+                if (t1 > 0) Console.WriteLine("{0} Moeda(s) de: R$ 1,00 real", t1);
+                if (m5 > 0) Console.WriteLine("{0} Moeda(s) de: R$ 0,50 centavos", m5);
+                if (m25 > 0) Console.WriteLine("{0} Moeda(s) de: R$ 0,25 centavos", m25);
+                if (m10 > 0) Console.WriteLine("{0} Moeda(s) de: R$ 0,10 centavos", m10);
+                if (m05 > 0) Console.WriteLine("{0} Moeda(s) de: R$ 0,05 centavos", m05);
+                if (m1 > 0) Console.WriteLine("{0} Moeda(s) de: R$ 0,01 centavos", m1);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
